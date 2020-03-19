26b8265836dee4f3e9ac3607d5ffd4e329c66b31
arifpatanduk <arifpatanduk2@gmail.com>
2020-03-20 03:38:25 +0700

    logo

--- hapus.php
+++ hapus.php
@@ -1,5 +1,19 @@
 <?php
 
-
+$a = 1;
+$b = 1;
+$c = 5;
+while ($a <= 5){
+    while ($b >= $a){
+        print '-';
+        $b++;
+    }
+    while ($c >= $n){
+        print '+';
+        $c--;
+    }
+    print '<br>';
+    $a++;
+}
 
 ?>
